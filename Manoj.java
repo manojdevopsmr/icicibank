4b8db1e312258e55fe2f3b8831d73a909a3091d1
manojdevopsmr <manojmrcloud@gmail.com>
2025-05-05 05:26:07 +0000

    for merging  have updated the file

--- Manoj.java
+++ Manoj.java
@@ -1,5 +1,5 @@
 I am doing for practise purpose 
 For github
 Now i am updating this file for my idea..
-Hello i am updatig in development branch
+Hello i am updatig in development different branch
 
